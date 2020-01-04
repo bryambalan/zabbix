4d78f3ccc2c59bee8632f45eb1a02cb600ab0c23
made4-it <59491217+made4-it@users.noreply.github.com>
2020-01-03 21:06:17 -0300

    Script finalizado.
    Script que irá ler o XML da pagina do down detector e com base no status code da pagina, irá informar o estado o site em questão.

--- Scripts/down.py
+++ Scripts/down.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python
+
+from lxml import html
+import requests
+import re
+import sys
+
+def convertTuple(tup):
+    str =  ''.join(tup)
+    return str
+
+if len(sys.argv) < 2:
+    print("erro, informe o site a ser testado\n")
+    sys.exit(1)
+site = sys.argv[1]
+
+url = 'https://downdetector.com.br/fora-do-ar/' + site + '/'
+
+page = requests.get(url)
+if page.status_code != 200:
+    #print("ERROR-404")
+    print("0")
+    sys.exit(1)
+
+tree = html.fromstring(page.content)
+status = tree.xpath('//*[@id="chart-row"]/div/div/script[1]/text()')
+#print(status)
+
+for x in range(len(status)):
+    data = status[x]
+    #print(data)
+    teste = re.compile(".*status: '(.*)',.*", re.MULTILINE)
+    for match in teste.finditer(data):
+        status = match.groups()
+        status = convertTuple(status)
+	      #print(status)
+  	    #Conventendo String para Valor numerico
+        if(status == 'success'):
+            status = 10
+        if(status == 'warning'):
+            status = 20
+        if(status == 'danger'):
+            status = 30
+        print(status)
